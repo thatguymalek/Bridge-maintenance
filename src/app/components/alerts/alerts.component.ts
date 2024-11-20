c0666b728b4da7aa34c317392883331d8bea7da3
Malek <chayoukhimalek@gmail.com>
2024-11-20 16:16:43 +0100

    Describe the changes made

---
+++ src/app/components/alerts/alerts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alerts',
+  imports: [],
+  templateUrl: './alerts.component.html',
+  styleUrl: './alerts.component.css'
+})
+export class AlertsComponent {
+
+}
